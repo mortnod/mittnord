b5de78a23389421683dc5199a696570bf1e8a953
Morten Noddeland <morten@noddeland.no>
2020-10-20 22:19:23 +0200

    Improve radioCard shadow

--- constants/theme.js
+++ constants/theme.js
@@ -80,8 +80,8 @@ const shadows = {
   '0': 'none',
   '1': '0 0 0 1px rgba(0, 0, 0, 0.05)',
   '2': '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
-  '3': '0 1px 3px 0 rgba(0, 0, 0, 0.05), 0 1px 1px 0 rgba(0, 0, 0, 0.15)', // Modifisert Tailwind
-  '4': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+  '3': '0 1px 3px 0 rgba(0, 0, 0, 0.05), 0 1px 1px 0 rgba(0, 0, 0, 0.15)',
+  '4': '0 1px 1px 0 rgba(0, 0, 0, 0.2), 0 2px 4px 0px rgba(0, 0, 0, 0.1)',
   '5':
     '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
   '6':
