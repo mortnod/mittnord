f25eb078c6c80e9d279bc7ec4f4539f4bec4ffce
Morten Noddeland <morten@noddeland.no>
2021-03-07 14:02:42 +0100

    Radio card: Fix bug where Firefox doesn't respect display: flex on legend elements

--- components/radioCard/radioCardGroup.js
+++ components/radioCard/radioCardGroup.js
@@ -4,29 +4,31 @@ import { jsx } from 'theme-ui';
 const RadioCardGroup = ({ legend, icon, children }) => {
   return (
     <fieldset sx={{ border: 'none', mb: 10 }}>
-      <legend
-        sx={{
-          display: 'flex',
-          alignItems: 'center',
-          fontSize: 'md',
-          fontWeight: '600',
-          mb: '4',
-        }}
-      >
-        {!!icon && (
-          <div
-            sx={{
-              color: 'muted',
-              fontSize: '18px',
-              mr: 3,
-              display: 'flex',
-              alignItems: 'center',
-            }}
-          >
-            {icon}
-          </div>
-        )}
-        {legend}
+      <legend>
+        <span
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            fontSize: 'md',
+            fontWeight: '600',
+            mb: '4',
+          }}
+        >
+          {!!icon && (
+            <div
+              sx={{
+                color: 'muted',
+                fontSize: '18px',
+                mr: 3,
+                display: 'flex',
+                alignItems: 'center',
+              }}
+            >
+              {icon}
+            </div>
+          )}
+          {legend}
+        </span>
       </legend>
       <div sx={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: 2 }}>
         {children}
