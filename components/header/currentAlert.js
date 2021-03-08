a6e47c65101a34d21cdb5e503a0ca2f8867b25f6
Morten Noddeland <morten@noddeland.no>
2021-03-08 21:31:29 +0100

    Alert: Add fadeIn effect to make the alert appearing less jarring

--- components/header/currentAlert.js
+++ components/header/currentAlert.js
@@ -2,20 +2,23 @@
 import { jsx } from 'theme-ui';
 
 import { withTranslation } from '../../i18n';
+import FadeIn from '../../utils/fadeIn';
 import Alert from '../alert/alert';
 import ClientOnly from '../clientOnly/clientOnly';
 
 const CurrentAlert = ({ t }) => {
   return (
     <ClientOnly>
-      <Alert
-        href={t('alert-href')}
-        startAt={new Date('2021-03-06')}
-        endAt={new Date('2022-02-01')}
-        important
-      >
-        {t('alert-content')}
-      </Alert>
+      <FadeIn>
+        <Alert
+          href={t('alert-href')}
+          startAt={new Date('2021-03-06')}
+          endAt={new Date('2022-02-01')}
+          important
+        >
+          {t('alert-content')}
+        </Alert>
+      </FadeIn>
     </ClientOnly>
   );
 };
---
+++ utils/fadeIn.js
@@ -0,0 +1,30 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
+
+const FadeIn = ({ duration = 300, delay = 0, children }) => {
+  return (
+    <div
+      sx={{
+        '@keyframes fadeIn': {
+          from: {
+            opacity: 0,
+          },
+          to: {
+            opacity: 1,
+          },
+        },
+
+        '@media (prefers-reduced-motion: no-preference)': {
+          animationName: 'fadeIn',
+          animationFillMode: 'backwards',
+          animationDuration: `${duration}ms`,
+          animationDelay: `${delay}ms`,
+        },
+      }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default FadeIn;
