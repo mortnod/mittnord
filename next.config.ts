5527149594b0aa784db4d95f3353b12b83567fce
Morten Noddeland <morten@noddeland.no>
2025-03-10 22:37:17 +0100

    Add service worker using Serwist

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,17 @@
+import withSerwistInit from '@serwist/next';
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
+const withSerwist = withSerwistInit({
+  swSrc: 'components/sw.ts',
+  swDest: 'public/sw.js',
+});
+
+const nextConfig: NextConfig = withSerwist({
   i18n: {
     locales: ['nb', 'en'],
     defaultLocale: 'nb',
     localeDetection: false,
   },
-};
+});
 
 export default nextConfig;
