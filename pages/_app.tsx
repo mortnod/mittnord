e925611ddef206909ec883402cbc9fe70c16848e
Morten Noddeland <morten@noddeland.no>
2025-04-22 20:57:09 +0200

    Add Posthog reverse proxy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,8 +18,8 @@ function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
-      api_host:
-        process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://eu.i.posthog.com',
+      api_host: '/ingest',
+      ui_host: 'https://eu.posthog.com',
       person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
       // Enable debug mode in development
       loaded: (ph) => {
